f48e81eb8d709fc48d78a1bb2405381ef52b808f
Alexis Moreau <moreau.alexis@morman.fr>
2018-01-17 21:17:55 +0100

    opacity

--- particle_dark/assets/js/custom/particle-theme-light.js
+++ particle_dark/assets/js/custom/particle-theme-light.js
@@ -70,6 +70,118 @@
         time: 2200
     });
 
+    // Particles
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 123,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "FFFFFF"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#FFFFFF"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "FFFFFF",
+                "opacity": 0.5,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 4,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 231,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 231,
+                    "size": 12,
+                    "duration": 1.542946703372556,
+                    "opacity": 0.9,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 231,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
+
     // Typed JS
     $(".skilltechtypetext").typed({
         // This is were you need to change the text
@@ -84,55 +196,6 @@
 
 })(jQuery);
 
-"use strict";
-// YouTube
-// This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-//  This function creates an <iframe> (and YouTube player)
-//   after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('yt-player', {
-        // This is where you paste video's ID
-        videoId: videoId,
-        playerVars: {
-            'autoplay': 1,
-            'controls': 0,
-            'autohide':1,
-            'enablejsapi':1,
-            'loop':1,
-            'disablekb':1,
-            'fs': 0,
-            'modestbranding': 0,
-            'showinfo': 0,
-            'color': 'white',
-            'theme': 'dark',
-            'rel':0
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-// The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
-    player.mute();
-    player.setVolume(0);
-    player.setLoop(true);
-}
-// The API will call this function when the video player's state changes.
-function onPlayerStateChange(event) {
-    if (event.data === 0) {
-        player.playVideo();
-    }
-}
 
 
 
