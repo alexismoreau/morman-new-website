527a0ff3a2dbab2ae2cc07c711ceb5eebbab3c9f
Alexis Moreau <moreau.alexis@morman.fr>
2018-07-02 10:10:01 -0400

    offres js

---
+++ particle_dark/assets/js/custom/offres.js
@@ -0,0 +1,55 @@
+$( "#visibility" ).on( "mouseover", function() {
+  $('#modal-visibility').css({opacity: 1});
+});
+$( "#visibility" ).on( "mouseleave", function() {
+  $('#modal-visibility').css({opacity: 0});
+});
+
+$( "#referencement" ).on( "mouseover", function() {
+  $('#modal-referencement').css({opacity: 1});
+});
+$( "#referencement" ).on( "mouseleave", function() {
+  $('#modal-referencement').css({opacity: 0});
+});
+
+$( "#multisupport" ).on( "mouseover", function() {
+  $('#modal-multisupport').css({opacity: 1});
+});
+$( "#multisupport" ).on( "mouseleave", function() {
+  $('#modal-multisupport').css({opacity: 0});
+});
+
+$( "#mobileapp" ).on( "mouseover", function() {
+  $('#modal-mobileapp').css({opacity: 1});
+});
+$( "#mobileapp" ).on( "mouseleave", function() {
+  $('#modal-mobileapp').css({opacity: 0});
+});
+
+$( "#ux" ).on( "mouseover", function() {
+  $('#modal-ux').css({opacity: 1});
+});
+$( "#ux" ).on( "mouseleave", function() {
+  $('#modal-ux').css({opacity: 0});
+});
+
+$( "#loyalty" ).on( "mouseover", function() {
+  $('#modal-loyalty').css({opacity: 1});
+});
+$( "#loyalty" ).on( "mouseleave", function() {
+  $('#modal-loyalty').css({opacity: 0});
+});
+
+$( "#data" ).on( "mouseover", function() {
+  $('#modal-data').css({opacity: 1});
+});
+$( "#data" ).on( "mouseleave", function() {
+  $('#modal-data').css({opacity: 0});
+});
+
+$( "#forecast" ).on( "mouseover", function() {
+  $('#modal-forecast').css({opacity: 1});
+});
+$( "#forecast" ).on( "mouseleave", function() {
+  $('#modal-forecast').css({opacity: 0});
+});
